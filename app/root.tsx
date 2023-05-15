43e8737d953a9eba72855858153547d6fe1a9d5b
Mikku44 <104062911+Mikku44@users.noreply.github.com>
2023-05-15 14:49:27 +0700

    sizing

--- app/root.tsx
+++ app/root.tsx
@@ -206,7 +206,7 @@ export default function App() {
 </div>
          
           <div className="flex justify-around flex-wrap">
-            <div className="box-content h-64 sm:w-1/2 w-128 drop-shadow-xl m-10 rounded-2xl original   bg-black " id='pad'>  <div className="box-content h-10 w-10 flex items-center float-center "> <img className="p-1" src="./pictures/Asset 1 white@4x.png" alt="" /></div> </div>
+            <div className="box-content sm:h-64 sm:w-1/2 h-24 w-full  drop-shadow-xl m-10 rounded-2xl original    bg-black " id='pad'>  <div className="box-content h-10 w-10 flex items-center float-center "> <img className="p-1" src="./pictures/Asset 1 white@4x.png" alt="" /></div> </div>
             <div className="block">
               <div className="font-bold">SELECT BACKGROUND COLOR</div>
               <div className="flex">
